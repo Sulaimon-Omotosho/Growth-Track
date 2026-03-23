62de6ecdda2ceb0aadacfc6c480ff3afb26e53bc
Sulaimon Omotosho <susudef2u@gmail.com>
2026-03-23 21:41:10 +0100

    Chore: Api deployment debugging

--- packages/auth-db/lib/prisma.ts
+++ packages/auth-db/lib/prisma.ts
@@ -1,8 +1,6 @@
 import 'dotenv/config'
-import { PrismaPg } from '@prisma/adapter-pg.js'
-// import { PrismaPg } from '@prisma/adapter-pg.'
-import { PrismaClient } from '../generated/prisma/client.js'
-// import { PrismaClient } from '../generated/prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg.'
+import { PrismaClient } from '../generated/prisma/client'
 
 const connectionString = `${process.env.DATABASE_URL}`
 
